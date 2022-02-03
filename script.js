14a1a1b649679273ee02dfeb407100bfc303bc91
sachinnabar <sach5979@hotmail.com>
2022-02-02 22:20:08 -0500

    added files.

---
+++ script.js
@@ -0,0 +1,18 @@
+d3.selectAll("body").on("change", updatePage);
+
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
+  
+  init();
+  
