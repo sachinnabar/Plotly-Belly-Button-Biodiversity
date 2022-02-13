54d1d23acfbba5e5b54cd935b9049609a36140a7
sachinnabar <sach5979@hotmail.com>
2022-02-13 12:06:04 -0500

    added files.

--- script.js
+++ script.js
@@ -15,4 +15,31 @@ function init() {
   })}
   
   init();
+
+  function optionChanged(newSample) {
+    console.log(newSample);
+  }
+  
+  function optionChanged(newSample) {
+    buildMetadata(newSample);
+    buildCharts(newSample);
+  }
+  
+  function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      var PANEL = d3.select("#sample-metadata");
+  
+      PANEL.html("");
+      PANEL.append("h6").text(result.ethnicity);
+      PANEL.append("h6").text(result.gender);
+      PANEL.append("h6").text(result.age);
+      PANEL.append("h6").text(result.location);
+      PANEL.append("h6").text(result.bbtype);
+      PANEL.append("h6").text(result.wfreq);
+  
+    });
+  }
   
